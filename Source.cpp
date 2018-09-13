7c750830059fd3fb71e2d0eba026487d92b557a3
zxc <zxc@gmail.com>
2018-09-13 20:44:50 +0800

    stitching test

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include <opencv2/stitching.hpp>
+//#include <opencv2/stitching.hpp>
 
 using namespace cv;
 using namespace std;
